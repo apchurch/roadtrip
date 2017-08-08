ada219a4b04330f0ba29e514559b27e5a934571c
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-08-08 17:58:32 -0600

    Add existing file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(request, response){
-	response.sendFile('./index.html', {root: './public'})
+	response.sendFile('./index.html', {root: './public/html'})
 })
 // app.get('/main.css', function(request, response){
 // 	response.sendFile('./main.css', {root: './public'})
@@ -14,19 +14,19 @@ app.use(express.static('public'))
 // 	response.sendFile('./main.js', {root: './public'})
 // })
 app.get('/newyork', function(request, response){
-	response.sendFile('./newyork.html', {root: './public'})
+	response.sendFile('./newyork.html', {root: './public/html'})
 })
 app.get('/atlanta', function(request, response){
-	response.sendFile('./atlanta.html', {root: './public'})
+	response.sendFile('./atlanta.html', {root: './public/html'})
 })
 app.get('/miami', function(request, response){
-	response.sendFile('./miami.html', {root: './public'})
+	response.sendFile('./miami.html', {root: './public/html'})
 })
 app.get('/neworleans', function(request, response){
-	response.sendFile('./neworleans.html', {root: './public'})
+	response.sendFile('./neworleans.html', {root: './public/html'})
 })
 app.get('/boulder', function(request, response){
-	response.sendFile('./boulder.html', {root: './public'})
+	response.sendFile('./boulder.html', {root: './public/html'})
 })
 
 // app.use(express.static())
